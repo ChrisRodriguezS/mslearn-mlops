cc2527d2fa83ca644518cb8a5acc53d2fa9aa903
ChrisRodriguezS <christianrodriguez@coca-cola.com>
2025-04-16 14:59:06 +0000

    feat: initial data processing train script

--- src/model/train.py
+++ src/model/train.py
@@ -1,5 +1,4 @@
 # Import libraries
-
 import argparse
 import glob
 import os
@@ -7,11 +6,15 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import mlflow
 
+DF_COLUMNS = ['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']
 
 # define functions
 def main(args):
     # TO DO: enable autologging
+    mlflow.autolog()
 
 
     # read data
@@ -34,7 +37,9 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-
+def split_data(df):
+    X, y = df[DF_COLUMNS].values, df['Diabetic'].values
+    return  train_test_split(X, y, test_size=0.30, random_state=0)
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
